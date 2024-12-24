38333fb41a5b9e8e521790e1b7b5f4e587e44a8b
xdearboy <105206420+xdearboy@users.noreply.github.com>
2024-12-24 15:43:37 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { JetBrains_Mono } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin', 'cyrillic'] })
 
